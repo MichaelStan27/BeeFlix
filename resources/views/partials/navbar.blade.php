43561f4813dd28956521e8c4e96310ac37e6b53e
Michael Stanley <michstan96@gmail.com>
2022-05-26 07:20:18 +0700

    Update back button logic method on navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-white shadow-lg py-2 px-4">
     <h1 class="font-light text-3xl">BeeFlix</h1>
     <ul class="flex">
-        <a href="{{ URL::previous() }}">
+        <a href="{{ url()->previous() }}">
             <li class="border-2 bg-black text-white p-1 border-gray-600 text-sm font-medium hover:scale-105 shadow-md">BACK</li>
         </a>
         <a href="{{ route('home') }}">
